7485c3143de5801fe93f95781494559e86021ede
AlexJr <alejandroriglesias@gmail.com>
2014-06-02 16:14:50 +0200

    Visualization tool script modified

--- Scripts/visualization_tool.pl
+++ Scripts/visualization_tool.pl
@@ -4,7 +4,8 @@ use warnings;
 use Bio::DB::SeqFeature::Store;
 use Bio::SeqFeature::Generic;
 
-
+#my $ID = <stdin>;
+my $ID = $ARGV[0];
  # Open the sequence database
  my $db      = Bio::DB::SeqFeature::Store->new( -adaptor => 'DBI::mysql',
                                                 -dsn     => 'dbi:mysql:Fungal3endprocessing',
@@ -17,39 +18,33 @@ use Bio::SeqFeature::Generic;
 #  while (my $feature = $iterator->next_seq) {
 #    print $feature->primary_tag,' ',$feature->display_name,"\n";
 #  }
-my @features = $db->get_features_by_type("polypeptide");
-# my @features = $db->features(-seq_id => 'CADAFLAT00007143|mRNA');
-# my $seq = $db->get_Seq_by_id('CADAFLAT00007143|mRNA');
-my $thisfeature = shift @features;
+#my @features = $db->get_features_by_type("polypeptide");
+## my @features = $db->features(-seq_id => 'CADAFLAT00007143|mRNA');
+## my $seq = $db->get_Seq_by_id('CADAFLAT00007143|mRNA');
+my @fulllengthfeatures = $db->features(-seqid => $ID, -type => "polypeptide");		#Sequence ID is introduced in this line
+my $FULLLENGTH = shift @fulllengthfeatures;
+my $seqlength = $FULLLENGTH->seq->length;
+
+my @features = $db->get_features_by_location($ID);		#Sequence ID is introduced in this line
+
+my $thisfeature = $features[0];
 
 #my $seq_id =$thisfeature->ref;
-my $seq = $thisfeature->seq;
-my $id = $thisfeature->id;
-my $start = $thisfeature->start;
-my $end = $thisfeature->end;
+#my $seq = $thisfeature->seq;
+#my $id = $thisfeature->id;
+#my $start = $thisfeature->start;
+#my $end = $thisfeature->end;
 
-@features = $db->get_features_by_location('YOR250C', $start, $end);
+
 
 my $seqobj = Bio::Seq->new(-id => $thisfeature->load_id, 
 			   -display_id => $thisfeature->load_id,
-				-seq => $thisfeature->seq->seq);
+				-seq => $FULLLENGTH->seq->seq);
 
 #my @features = $seq->features;
-my $ID=0;
+$ID=0;
 foreach my $feature(@features){
     ++$ID;
-	my $compliant_feature = Bio::SeqFeature::Generic->new(
-	-primary_id => $feature->primary_id,
-	-start => $feature->start,
-	-end => $feature->end,
-	-strand => $feature->strand,
-	-display_name => $feature->display_name,
-	-seq_id => $feature->seq_id,
-	-type => $feature->type.$ID,
-	-source_tag => $feature->source_tag.$ID,
-	-primary_tag => $feature->primary_tag.$ID,
-	);
-	
 	my $term = " ";
 	if ($feature->attributes('signature_desc')){
 		my @terms = $feature->attributes('signature_desc');
@@ -79,6 +74,21 @@ foreach my $feature(@features){
          #   }
         #}   
 	print "ID ", $feature->display_name, " Type ", $feature->type, " name " , $feature->name, " start ", $feature->start, "  end ", $feature->end, " DESC: ", $term, "\n";
+
+	my $compliant_feature = Bio::SeqFeature::Generic->new(
+	-primary_id => $feature->primary_id,
+	-start => $feature->start,
+	-end => $feature->end,
+	-strand => $feature->strand,
+	-display_name => $feature->display_name,
+	-seq_id => $feature->seq_id,
+	-type => $feature->type.$ID,
+	-source_tag => $feature->source_tag.$ID,
+	-primary_tag => $feature->primary_tag.$ID,
+	);
+	$compliant_feature->{term} = $term;
+	
+
 	$seqobj->add_SeqFeature($compliant_feature);
 
 }
@@ -108,7 +118,7 @@ foreach my $feature(@features){
                $seqobj,
                -orientation => 'horizontal',
                label => 'primary_tag',
-               width => 200,
+               width => $seqlength,
                );
         $MW->bind("<Button-1>" => sub {
             my ($FID, $strand, $source, $type, $canvas, $DB_ID) = $MapObj->getSelectedTags;
@@ -122,6 +132,7 @@ foreach my $feature(@features){
                 my $feature = $hash->{$key};
                 #print $feature, $key;
                 print $feature->display_name, "\n";
+		print $feature->start, "\t", $feature->end, "\t", $feature->{term},"\n";
              }
            });
 
